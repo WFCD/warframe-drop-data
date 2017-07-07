a13a13d6824e3b9be6ca66959eafadd6934b627e
Christopher Kaster <me@atomicptr.de>
2017-07-07 17:29:43 +0200

    add some more structure and basic regex

--- generate.js
+++ generate.js
@@ -1,1 +1,29 @@
-console.log("yolo");
+const cheerio = require("cheerio")
+const request = require("request")
+
+const {missionRewards} = require("./lib/data.js")
+
+const config = {
+    json: true,
+    yaml: true,
+
+    dropDataUrl: "https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html"
+}
+
+console.log("### Warframe Drop Data by https://github.com/atomicptr\n")
+console.log(`Requesting Drop Data from ${config.dropDataUrl}...\n`)
+
+request(config.dropDataUrl, (err, res, body) => {
+    if(err) {
+        console.error(err)
+        return
+    }
+
+    console.log(`Response: ${res.statusCode}\n\t${res.statusMessage}`)
+
+    const $ = cheerio.load(body)
+
+    const missionRewardsTable = missionRewards($)
+
+
+})
