03bdc2a51d53270b70f3cd81b9002d251d2f2f90
Christopher Kaster <atomicptr@gmail.com>
2017-07-08 00:57:56 +0200

    fix paths

--- generateData.js
+++ generateData.js
@@ -1,6 +1,7 @@
 const cheerio = require("cheerio")
 const request = require("request")
 const crypto = require("crypto")
+const path = require("path")
 
 const fs = require("fs")
 
@@ -35,10 +36,12 @@ request(config.dropDataUrl, (err, res, body) => {
         console.log(`Old hash found: ${oldHash}`)
     }
 
-    if(hash === oldHash || forceRegeneration) {
-        // TODO: nothing new, close
-        console.log("Data hasn't changed, exit process.")
-        process.exit(0)
+    if(!config.forceRegeneration) {
+        if(hash === oldHash) {
+            // TODO: nothing new, close
+            console.log("Data hasn't changed, exit process.")
+            process.exit(0)
+        }
     }
 
     const $ = cheerio.load(body)
@@ -60,51 +63,55 @@ request(config.dropDataUrl, (err, res, body) => {
         timestamp: (new Date().getTime())
     }
 
-    trymkdir("./data")
+    trymkdir(path.resolve(__dirname, "data"))
 
     let jsonFormat = config.jsonMinify ? "" : "    "
 
     console.log("Writing... /data/all.json")
-    fs.writeFileSync("./data/all.json", JSON.stringify(data, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "all.json"), JSON.stringify(data, null, jsonFormat))
 
     console.log("Writing... /data/info.json")
-    fs.writeFileSync("./data/info.json", JSON.stringify(info, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "info.json"), JSON.stringify(info, null, jsonFormat))
 
     console.log("Writing... /data/missionRewards.json")
-    fs.writeFileSync("./data/missionRewards.json", JSON.stringify({missionRewards: data.missionRewards}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "missionRewards.json"), JSON.stringify({missionRewards: data.missionRewards}, null, jsonFormat))
 
     console.log("Writing... /data/relics.json")
-    fs.writeFileSync("./data/relics.json", JSON.stringify({relics: data.relics}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "relics.json"), JSON.stringify({relics: data.relics}, null, jsonFormat))
 
     console.log("Writing... /data/transientRewards.json")
-    fs.writeFileSync("./data/transientRewards.json", JSON.stringify({transientRewards: data.transientRewards}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "transientRewards.json"), JSON.stringify({transientRewards: data.transientRewards}, null, jsonFormat))
 
     console.log("Writing... /data/modLocations.json")
-    fs.writeFileSync("./data/modLocations.json", JSON.stringify({modLocations: data.modLocations}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "modLocations.json"), JSON.stringify({modLocations: data.modLocations}, null, jsonFormat))
 
     console.log("Writing... /data/enemyModTables.json")
-    fs.writeFileSync("./data/enemyModTables.json", JSON.stringify({enemyModTables: data.enemyModTables}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "enemyModTables.json"), JSON.stringify({enemyModTables: data.enemyModTables}, null, jsonFormat))
 
     console.log("Writing... /data/blueprintLocations.json")
-    fs.writeFileSync("./data/blueprintLocations.json", JSON.stringify({blueprintLocations: data.blueprintLocations}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "blueprintLocations.json"), JSON.stringify({blueprintLocations: data.blueprintLocations}, null, jsonFormat))
 
     console.log("Writing... /data/enemyBlueprintTables.json")
-    fs.writeFileSync("./data/enemyBlueprintTables.json", JSON.stringify({enemyBlueprintTables: data.enemyBlueprintTables}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "enemyBlueprintTables.json"), JSON.stringify({enemyBlueprintTables: data.enemyBlueprintTables}, null, jsonFormat))
 
     console.log("Writing... /data/sortieRewards.json")
-    fs.writeFileSync("./data/sortieRewards.json", JSON.stringify({sortieRewards: data.sortieRewards}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "sortieRewards.json"), JSON.stringify({sortieRewards: data.sortieRewards}, null, jsonFormat))
 
     console.log("Writing... /data/keyRewards.json")
-    fs.writeFileSync("./data/keyRewards.json", JSON.stringify({keyRewards: data.keyRewards}, null, jsonFormat))
+    fs.writeFileSync(path.resolve(__dirname, "data", "keyRewards.json"), JSON.stringify({keyRewards: data.keyRewards}, null, jsonFormat))
+
+    trymkdir(path.resolve(__dirname, `data`, `missionRewards`))
 
     // write structure
     for(let planet of Object.keys(data.missionRewards)) {
         for(let location of Object.keys(data.missionRewards[planet])) {
+            trymkdir(path.resolve(__dirname, `data`, `missionRewards`, `${planet}`))
+
             console.log(`Writing... /data/missionRewards/${planet}/${location}.json`)
             let missionData = Object.assign({}, data.missionRewards[planet][location])
             missionData.planet = planet
             missionData.location = location
-            fs.writeFileSync(`./data/missionRewards/${planet}/${location}.json`, JSON.stringify(missionData, null, jsonFormat))
+            fs.writeFileSync(path.resolve(__dirname, `data`, `missionRewards`, `${planet}`, `${location}.json`), JSON.stringify(missionData, null, jsonFormat))
         }
     }
 
@@ -112,8 +119,8 @@ request(config.dropDataUrl, (err, res, body) => {
     let relicStruct = {}
 
     for(let relic of data.relics) {
-        trymkdir(`./data/relics/`)
-        trymkdir(`./data/relics/${relic.tier}/`)
+        trymkdir(path.resolve(__dirname, `data`, `relics`))
+        trymkdir(path.resolve(__dirname, `data`, `relics`, `${relic.tier}`))
 
         if(!relicStruct[relic.tier]) {
             relicStruct[relic.tier] = {}
@@ -139,7 +146,7 @@ request(config.dropDataUrl, (err, res, body) => {
     for(let tier of Object.keys(relicStruct)) {
         for(let relicName of Object.keys(relicStruct[tier])) {
             console.log(`Writing... /data/relics/${tier}/${relicName}.json`)
-            fs.writeFileSync(`./data/relics/${tier}/${relicName}.json`, JSON.stringify(relicStruct[tier][relicName], null, jsonFormat))
+            fs.writeFileSync(path.resolve(__dirname, `data`, `relics`, `${tier}`, `${relicName}.json`), JSON.stringify(relicStruct[tier][relicName], null, jsonFormat))
         }
     }
 
