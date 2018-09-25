9ab6206db0a55c896400a936e86269c0e84b9bf2
Tobiah <tobiah@warframe.gg>
2018-09-25 15:07:14 -0500

    refactor to make stalker theme accessible (#36)
    * refactor to make stalker theme accessible
    
    * fix bad css
    
    * make sun white on stalker mode
    
    * better styling for stalker theme indicator

--- themes.js
+++ themes.js
@@ -1,3 +1,4 @@
+/*globals $, localStorage  */
 $(document).ready(function() {
     if(localStorage.getItem('_theme')) {
         $('html').addClass(localStorage.getItem('_theme'))
@@ -6,11 +7,23 @@ $(document).ready(function() {
     $('.theme-mode').click(event => {
         const target = $(event.currentTarget)
         if (target.hasClass('sun')) {
-            localStorage.removeItem('_theme');
-            $('html').removeClass('dark')
-        } else {
-            localStorage.setItem('_theme', 'dark');
+            localStorage.removeItem('_theme')
+            $('html').removeClass();
+        } else if (target.hasClass('moon')) {
+            localStorage.setItem('_theme', 'dark')
             $('html').addClass('dark')
+        } else if (target.hasClass('stalker')) {
+            localStorage.setItem('_theme', 'stalker')
+            $('html').removeClass()
+            $('html').addClass('stalker')
+        }
+        
+        if (localStorage.getItem('_theme') === 'stalker') {
+            $('.logo').attr('src', '/misc/stalker.png')
+            $('html').removeClass()
+            $('html').addClass('stalker')
+        } else {
+            $('.logo').attr('src', '/misc/logo.short.png')
         }
     })
     
