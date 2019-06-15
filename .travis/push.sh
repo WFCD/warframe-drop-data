06f6a76ae4136fbc0bfeaa7bf1f4b33190b0fb0a
Tobiah <tobiah@protonmail.com>
2019-06-15 01:22:03 -0500

    Travis Automated Builds (#54)
    * set env build steps for continuing travis build
    
    * whoops, don't commit build for that!
    
    * additional build steps
    
    commit with push script
    
    * Create push.sh
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * write status to file, cat file to env var, run based on that

---
+++ .travis/push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+upload_files() {
+  git add -am "chore(automated): Travis Update drop data tables: ${date}
+  git remote add origin-update https://${GITHUB_TOKEN}@github.com/WFCD/warframe-drops-data.git
+  git push --quiet --set-upstream  origin-update $TRAVIS_BRANCH
+}
+
+setup_git
+upload_files
--- generateData.js
+++ generateData.js
@@ -41,6 +41,7 @@ request(config.dropDataUrl, (err, res, body) => {
         if(hash === oldHash) {
             // TODO: nothing new, close
             console.log("Data hasn't changed, exit process.")
+            fs.writeFileSync(".build_status", "halt")
             process.exit(0)
         }
     }
@@ -182,8 +183,7 @@ request(config.dropDataUrl, (err, res, body) => {
             fs.writeFileSync(path.resolve(__dirname, `data`, `relics`, `${tier}`, `${relicName}.json`), JSON.stringify(relicStruct[tier][relicName], null, jsonFormat))
         }
     }
-
-
+    fs.writeFileSync(".build_status", "continue")
 })
 
 function trymkdir(dir) {
