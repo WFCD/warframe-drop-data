a39b2f0a89aa3ceba2c2cca0249907a4e7239e99
Tobiah <tobiah@protonmail.com>
2019-07-08 17:43:38 -0400

    fix: use commit flags instead of add

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,7 +5,7 @@ setup_git() {
 }
 
 upload_files() {
-  git add -am "chore(automated): Travis Update drop data tables: ${date}"
+  git commit -am "chore(automated): Travis Update drop data tables: ${date}"
   git remote add origin-update https://${GITHUB_TOKEN}@github.com/WFCD/warframe-drop-data.git
   git push --quiet --set-upstream  origin-update $TRAVIS_BRANCH
 }
