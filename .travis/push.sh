7c373e87a707595d53ed51a35dae512277638f9b
Tobiah <tobiah@warframe.gg>
2019-07-16 09:36:24 -0500

    another attempt to get the build comitting

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,11 +4,17 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
+commit_changes() {
+  git checkout -b $TRAVIS_BRANCH
+  git add .
+  git commit -m "chore(automated): Travis Update drop data tables: ${date}"
+}
+
 upload_files() {
-  git add . && git commit -m "chore(automated): Travis Update drop data tables: ${date}"
-  git remote add origin-update https://tobitenno:${GITHUB_TOKEN}@github.com/WFCD/warframe-drop-data.git > /dev/null 2>&1
+  git remote add origin-update https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
   git push --quiet --set-upstream  origin-update $TRAVIS_BRANCH
 }
 
 setup_git
+commit_changes
 upload_files
