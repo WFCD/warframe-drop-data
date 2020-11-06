294ffe5e9375b362b2ccd0606e2ec02dae93de7f
Tobiah <tobiah@protonmail.com>
2020-11-05 23:50:42 -0600

    chore(ci): build on push, clean up dev deps

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const serveStatic = require('serve-static');
 
 const app = express();
-// app.use(serveStatic('./site', {'index': ['./site/index.html']}))
 app.use(express.static('site'))
 app.listen(process.env.PORT || 3000);
