303da375a5ec5a289d81a1c77d42f479af263c0c
Tobiah <tobiah@pm.me>
2023-01-17 01:08:02 -0600

    test ci

--- site/js/index.js
+++ site/js/index.js
@@ -18,7 +18,7 @@ String.prototype.isJSON = function() {
   return true
 }
 
-const CURR_SCRIPT_VERSION = 15;
+const CURR_SCRIPT_VERSION = 15; // never got bumped w/ zariman
 const INFO_DEFAULT = '{"hash":"clem"}';
 
 function updateData(fromVersion, toVersion) {
