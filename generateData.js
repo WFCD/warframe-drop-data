ec29365177b54c164e0306e4cd31205de83a242c
Matt <7128721+TobiTenno@users.noreply.github.com>
2023-08-01 06:07:27 -0500

    feat(esm): upgrade to esm (#82)

--- generateData.js
+++ generateData.js
@@ -1,19 +1,53 @@
-'use strict';
+import crypto from 'node:crypto';
+import { dirname, resolve } from 'node:path';
+import {
+  writeFileSync, mkdirSync, readFileSync,
+} from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
-const crypto = require('crypto');
-const path = require('path');
-const fs = require('fs');
+import * as cheerio from 'cheerio';
+import ProgressBar from 'progress';
 
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+import { formatSiteData } from './lib/utils.js';
 
-const ProgressBar = require('progress');
-require('colors');
+import missionRewards from './lib/missionRewards.js';
+import relics from './lib/relics.js';
+import transientRewards from './lib/transientRewards.js';
+import modLocations from './lib/modLocations.js';
+import enemyModTables from './lib/enemyModTables.js';
+import blueprintLocations from './lib/blueprintLocations.js';
+import enemyBlueprintTables from './lib/enemyBlueprintTables.js';
+import sortieRewards from './lib/sortieRewards.js';
+import keyRewards from './lib/keyRewards.js';
+import bountyRewards from './lib/bountyRewards.js';
+import syndicates from './lib/syndicates.js';
+import dropByAvatar from './lib/dropByAvatar.js';
+
+import infoJson from './data/info.json' assert {type: 'json'};
+
+import 'colors';
+
+const mappers = {
+  missionRewards,
+  relics,
+  transientRewards,
+  modLocations,
+  enemyModTables,
+  blueprintLocations,
+  enemyBlueprintTables,
+  sortieRewards,
+  keyRewards,
+  bountyRewards,
+  syndicates,
+  dropByAvatar,
+};
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const initProgress = (total) => {
   if (process.env.CI !== 'true') {
     return new ProgressBar(
-      `Writing: ${'['.green}:bar${']'.green} ${':current'.grey}/${':total'.green} ${':file'.brightGreen} (${':elapsed'.cyan}s/${':eta'.cyan}s)`,
+      `Writing: ${'['.green}:bar${']'.green} ${':current'.grey}/${':total'.green} ${':file'.bgGreen} (${':elapsed'.cyan}s/${':eta'.cyan}s)`,
       {
         incomplete: '-'.red,
         complete: '='.cyan,
@@ -30,8 +64,6 @@ const initProgress = (total) => {
 
 let bar;
 
-const { formatSiteData } = require('./lib/utils');
-
 const writeData = (data, key, format) => {
   // console.log(`Writing... /data/${key}.json`)
   bar.tick({ file: `/data/${key}.json` });
@@ -41,7 +73,7 @@ const writeData = (data, key, format) => {
 
   const bodyStr = JSON.stringify(body, null, format);
 
-  fs.writeFileSync(path.resolve(__dirname, 'data', `${key}.json`), bodyStr);
+  writeFileSync(resolve(__dirname, 'data', `${key}.json`), bodyStr);
 };
 
 const config = {
@@ -55,7 +87,7 @@ console.log(`${'Requesting Drop Data'.grey} from ${config.dropDataUrl.cyan}...`)
 
 function trymkdir(dir) {
   try {
-    fs.mkdirSync(dir);
+    mkdirSync(dir);
   } catch (ex) {
     // directory already exists, probably
   }
@@ -74,9 +106,8 @@ fetch(config.dropDataUrl)
 
     let oldHash = null;
 
-    if (fs.existsSync('./data/info.json')) {
-      const info = require('./data/info.json');
-      oldHash = info.hash;
+    if (infoJson) {
+      oldHash = infoJson.hash;
 
       console.log(`${'Old hash found:'.grey} ${oldHash}`);
     }
@@ -85,7 +116,7 @@ fetch(config.dropDataUrl)
       if (hash === oldHash) {
         // nothing new, close
         console.log('Data hasn\'t changed, exit process.'.grey);
-        fs.writeFileSync('.build_status', 'halt');
+        writeFileSync('.build_status', 'halt');
         process.exit(0);
       }
     }
@@ -93,34 +124,33 @@ fetch(config.dropDataUrl)
     const $ = cheerio.load(body);
 
     const data = {
-      missionRewards: require('./lib/missionRewards')($),
-      relics: require('./lib/relics')($),
-      transientRewards: require('./lib/transientRewards')($),
-      modLocations: require('./lib/modLocations')($),
-      enemyModTables: require('./lib/enemyModTables')($),
-      blueprintLocations: require('./lib/blueprintLocations')($),
-      enemyBlueprintTables: require('./lib/enemyBlueprintTables')($),
-      sortieRewards: require('./lib/sortieRewards')($),
-      keyRewards: require('./lib/keyRewards')($),
-      // miscItems: require('./lib/miscItems')($),
-      cetusBountyRewards: require('./lib/bountyRewards')($, 'cetusRewards'),
-      solarisBountyRewards: require('./lib/bountyRewards')($, 'solarisRewards'),
-      deimosRewards: require('./lib/bountyRewards')($, 'deimosRewards'),
-      zarimanRewards: require('./lib/bountyRewards')($, 'zarimanRewards'),
-      syndicates: await require('./lib/syndicates')(),
+      missionRewards: mappers.missionRewards($),
+      relics: mappers.relics($),
+      transientRewards: mappers.transientRewards($),
+      modLocations: mappers.modLocations($),
+      enemyModTables: mappers.enemyModTables($),
+      blueprintLocations: mappers.blueprintLocations($),
+      enemyBlueprintTables: mappers.enemyBlueprintTables($),
+      sortieRewards: mappers.sortieRewards($),
+      keyRewards: mappers.keyRewards($),
+      cetusBountyRewards: mappers.bountyRewards($, 'cetusRewards'),
+      solarisBountyRewards: mappers.bountyRewards($, 'solarisRewards'),
+      deimosRewards: mappers.bountyRewards($, 'deimosRewards'),
+      zarimanRewards: mappers.bountyRewards($, 'zarimanRewards'),
+      syndicates: await mappers.syndicates(),
 
       // drops by avatar
-      resourceByAvatar: require('./lib/dropByAvatar')($, 'resourceByAvatar', 'Resource Drop Chance'),
-      sigilByAvatar: require('./lib/dropByAvatar')($, 'sigilByAvatar', 'Sigil Drop Chance'),
-      additionalItemByAvatar: require('./lib/dropByAvatar')($, 'additionalItemByAvatar', 'Additional Item Drop Chance'),
+      resourceByAvatar: mappers.dropByAvatar($, 'resourceByAvatar', 'Resource Drop Chance'),
+      sigilByAvatar: mappers.dropByAvatar($, 'sigilByAvatar', 'Sigil Drop Chance'),
+      additionalItemByAvatar: mappers.dropByAvatar($, 'additionalItemByAvatar', 'Additional Item Drop Chance'),
     };
 
     // generate relics structure
     const relicStruct = {};
 
     for (const relic of data.relics) {
-      trymkdir(path.resolve(__dirname, 'data', 'relics'));
-      trymkdir(path.resolve(__dirname, 'data', 'relics', `${relic.tier}`));
+      trymkdir(resolve(__dirname, 'data', 'relics'));
+      trymkdir(resolve(__dirname, 'data', 'relics', `${relic.tier}`));
 
       if (!relicStruct[relic.tier]) {
         relicStruct[relic.tier] = {};
@@ -172,44 +202,44 @@ fetch(config.dropDataUrl)
       modified: date,
     };
 
-    trymkdir(path.resolve(__dirname, 'data'));
+    trymkdir(resolve(__dirname, 'data'));
 
     const jsonFormat = config.jsonMinify ? '' : '    ';
 
     // add to builds
     bar.interrupt('Adding build'.grey);
-    const builds = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data', 'builds', 'builds.json')));
+    const builds = JSON.parse(readFileSync(resolve(__dirname, 'data', 'builds', 'builds.json')).toString());
     if (builds.filter(build => build.hash === info.hash).length === 0) builds.push(info);
 
     bar.tick({ file: '/data/builds/builds.json' });
-    fs.writeFileSync(path.resolve(__dirname, 'data', 'builds', 'builds.json'), JSON.stringify(builds, null, jsonFormat));
+    writeFileSync(resolve(__dirname, 'data', 'builds', 'builds.json'), JSON.stringify(builds, null, jsonFormat));
 
     bar.tick({ file: `/data/builds/${info.hash}.json` });
-    fs.writeFileSync(path.resolve(__dirname, 'data', 'builds', `${info.hash}.json`), JSON.stringify(data, null, jsonFormat));
+    writeFileSync(resolve(__dirname, 'data', 'builds', `${info.hash}.json`), JSON.stringify(data, null, jsonFormat));
 
     bar.tick({ file: '/data/all.json' });
-    fs.writeFileSync(path.resolve(__dirname, 'data', 'all.json'), JSON.stringify(data, null, jsonFormat));
+    writeFileSync(resolve(__dirname, 'data', 'all.json'), JSON.stringify(data, null, jsonFormat));
 
     bar.tick({ file: 'data/all.slim.json' });
-    fs.writeFileSync(path.resolve(__dirname, 'data', 'all.slim.json'), JSON.stringify(dropSiteData, null, jsonFormat));
+    writeFileSync(resolve(__dirname, 'data', 'all.slim.json'), JSON.stringify(dropSiteData, null, jsonFormat));
 
     bar.tick({ file: 'data/info.json' });
-    fs.writeFileSync(path.resolve(__dirname, 'data', 'info.json'), JSON.stringify(info, null, jsonFormat));
+    writeFileSync(resolve(__dirname, 'data', 'info.json'), JSON.stringify(info, null, jsonFormat));
 
     Object.keys(data).forEach((key) => { writeData(data, key, jsonFormat); });
 
-    trymkdir(path.resolve(__dirname, 'data', 'missionRewards'));
+    trymkdir(resolve(__dirname, 'data', 'missionRewards'));
 
     // write structure
     for (const planet of Object.keys(data.missionRewards)) {
       for (const location of Object.keys(data.missionRewards[planet])) {
-        trymkdir(path.resolve(__dirname, 'data', 'missionRewards', `${planet}`));
+        trymkdir(resolve(__dirname, 'data', 'missionRewards', `${planet}`));
 
         bar.tick({ file: `/data/missionRewards/${planet}/${location.replace(':', '')}.json` });
         const missionData = { ...data.missionRewards[planet][location] };
         missionData.planet = planet;
         missionData.location = location;
-        fs.writeFileSync(path.resolve(__dirname, 'data', 'missionRewards', `${planet}`, `${location.replace(':', '')}.json`), JSON.stringify(missionData, null, jsonFormat));
+        writeFileSync(resolve(__dirname, 'data', 'missionRewards', `${planet}`, `${location.replace(':', '')}.json`), JSON.stringify(missionData, null, jsonFormat));
       }
     }
 
@@ -217,8 +247,8 @@ fetch(config.dropDataUrl)
     for (const tier of Object.keys(relicStruct)) {
       for (const relicName of Object.keys(relicStruct[tier])) {
         bar.tick({ file: `/data/relics/${tier}/${relicName}.json` });
-        fs.writeFileSync(path.resolve(__dirname, 'data', 'relics', `${tier}`, `${relicName}.json`), JSON.stringify(relicStruct[tier][relicName], null, jsonFormat));
+        writeFileSync(resolve(__dirname, 'data', 'relics', `${tier}`, `${relicName}.json`), JSON.stringify(relicStruct[tier][relicName], null, jsonFormat));
       }
     }
-    fs.writeFileSync('.build_status', 'continue');
+    writeFileSync('.build_status', 'continue');
   });
